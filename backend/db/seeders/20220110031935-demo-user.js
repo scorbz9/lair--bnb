9ff3951f73307ddeff8c7fb79c68c9175ea6e696
Seth Corbett <scorbz9@gmail.com>
2022-01-09 20:11:36 -0800

    configured for deploy

---
+++ backend/db/seeders/20220110031935-demo-user.js
@@ -0,0 +1,21 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
