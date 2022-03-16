b7237467f2dccb40a4c3f87b8aa457daf63a5356
Seth Corbett <scorbz9@gmail.com>
2022-03-16 12:10:49 -0700

    restyled signup/login

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, useHistory } from 'react-router-dom';
+import { Redirect, useHistory, Link } from 'react-router-dom';
 import './LoginForm.css';
 
 function LoginFormPage() {
@@ -36,30 +36,41 @@ function LoginFormPage() {
   return (
     <div id="log-in-container">
       <form id="login-form" onSubmit={handleSubmit}>
+          <div className="login-form-header">
+            <h4>Please log in to Lairbnb</h4>
+            <h6>By logging in to Lairbnb, you hereby agree to not sue us in the event of paranormal activity.</h6>
+          </div>
         <ul>
           {errors.map((error, idx) => <li key={idx}>{error}</li>)}
         </ul>
         <label>
-          Username or Email
           <input
             type="text"
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
             required
+            autoComplete='off'
+            className="login-form-input"
+            placeholder='Username or Email'
           />
         </label>
         <label>
-          Password
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            autoComplete='off'
+            className="login-form-input"
+            placeholder='Password'
           />
         </label>
-        <button id="log-in-submit" type="submit">Log In</button>
+        <button id="log-in-submit" className="login-form-button" type="submit">Log In</button>
+        <div className="login-form-sign-up-container">
+              <p className="login-form-sign-up">New to Lairbnb? <Link to="/signup" className="login-form-sign-up-link">Sign up</Link></p>
+        </div>
+        <button id="demo-link" className="login-form-button" onClick={handleDemo}>Demo User</button>
       </form>
-      <button id="demo-link" onClick={handleDemo}>Demo User</button>
     </div>
   );
 }
