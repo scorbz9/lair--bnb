252d941bae0c6e88abc4a96e693e2210cd60dfc4
Seth Corbett <scorbz9@gmail.com>
2022-04-27 19:16:17 -0700

    created bookings view

--- frontend/src/components/BookingsPage/index.js
+++ frontend/src/components/BookingsPage/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBookings } from "../../store/bookings";
 
+import { parseDate } from "../../utils";
+
 import './BookingsPage.css'
 
 const BookingsPage = () => {
@@ -12,7 +14,7 @@ const BookingsPage = () => {
     }, [])
 
     const bookings = useSelector(state => state.bookingsState.entries)
-    console.log(bookings)
+
 
 
     return (
@@ -20,19 +22,29 @@ const BookingsPage = () => {
             <h2 className="bookings-page__header">View your bookings</h2>
             <div className="bookings-page__bookings">
                 {bookings.map((booking, index) => {
+
+                    // Format datetime objects for display
+                    const parsedStartDate = parseDate(new Date(booking.startDate))
+                    const parsedEndDate = parseDate(new Date(booking.endDate))
+
                     return (
                         <div className="bookings-page__booking" key={index}>
                             <img src={booking.Spot.Images[0].imgURL} className="bookings-page__spot-image"/>
-                            <h3 className="bookings-page__spot-address">
-                                {booking.Spot.address}
-                            </h3>
-                            <p className="bookings-page__booking-date-range">
-                                {booking.startDate} - {booking.endDate}
-                            </p>
-                            <button className="bookings-page__edit-booking-button">
+                            <div className="bookings-page__booking-info">
+                                <h3 className="bookings-page__spot-address">
+                                    {booking.Spot.address}
+                                </h3>
+                                <p className="bookings-page__booking-date-range">
+                                    {parsedStartDate} - {parsedEndDate}
+                                </p>
+                                <p className="bookings-page__spot-description">
+                                    {booking.Spot.description}
+                                </p>
+                            </div>
+                            <button className="bookings-page__button bookings-page__button--edit">
                                 Edit
                             </button>
-                            <button className="bookings-page__delete-booking-button">
+                            <button className="bookings-page__button bookings-page__button--delete">
                                 Delete
                             </button>
                         </div>
